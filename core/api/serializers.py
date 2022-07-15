292b9cd2ec24d19e7905f3e9c7faf8c1f6647d3a
leone <leonesilva.10@gmail.com>
2022-07-15 17:30:54 -0300

    ManytoMany relationships - Correção & Foreignkeys relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework.serializers import ModelSerializer
+
+from atracoes.models import Atracao
 from core.models import PontoTuristico
 from enderecos.api.serializers import EnderecoSerializer
 from atracoes.api.serializers import AtracaoSerializer
 from avaliacoes.api.serializers import AvaliacaoSerializer
+from enderecos.models import Endereco
 
 
 class PontoTuristicoSerializer(ModelSerializer):
@@ -20,13 +23,22 @@ class PontoTuristicoSerializer(ModelSerializer):
 
     def cria_atracoes(self, atracoes, ponto):
         for atracao in atracoes:
-            at = Atracao.objects,create(**atracao)
+            at = Atracao.objects.create(**atracao)
             ponto.atracoes.add(at)
 
-    def create(self, validate_data):
+    def create(self, validate_data, validated_data=None):
         atracoes = validated_data['atracoes']
         del validate_data['atracoes']
+
+        endereco = validate_data['enderecos']
+        del validate_data['endereco']
+
         ponto = PontoTuristico.objects.create(**validate_data)
         self.cria_atracoes(atracoes, ponto)
+
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
+
+        ponto.save()
 
         return ponto
